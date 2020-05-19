6f84e719527817a9b1cc6ef2e69828a2f764205c
bradleywh1 <bradley.wh@outlook.com>
2020-05-20 10:57:48 +1200

    credential

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Sidebar = () => (
     <div className="sidebar" data-testid="sidebar">
-        <ul>
+        <ul className="sidebar__generic">
             <li>Inbox</li>
             <li>Today</li>
             <li>Next 7 days</li>
