3e1ce4c8f6cb0aa4d9bea91cf49fefc5d3e0a6e3
Bradley Wheeler <bradley.wh@outlook.com>
2020-05-19 16:53:21 +1200

    cleaning up boiler plate

---
+++ src/components/layout/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header" data-testid="header">
+            <p>Header</p>
+        </header>
+    )
+};
