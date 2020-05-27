984988afffaf0e210173dee6b8e55557a73d642c
bradleywh1 <bradley.wh@outlook.com>
2020-05-27 17:09:48 +1200

    firebase logic

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,27 @@
 import { useState, useEffect } from 'react';
 import { firebase } from '../firebase';
 
+
+collatedTasksExists = () => {};
+
 export const useTasks = selectedProject => {
     const [tasks, setTasks] = useState([]);
-}
+
+    useEffect(() => {
+        let unsubscribe = firebase
+            .firestore()
+            .collection('tasks')
+            .where('userId', '==', '1')
+
+        unsubscribe = selectedProject && !collatedTasksExists(selectedProject) ?
+        (unsubscribe = unsubscribe.where('projectId', '==', 'selectedProject'))
+        : selectedProject === 'TODAY'
+        ? (unsubscribe = unsubscribe.where('date', '==', moment().format('DD/MM/YYYY')))
+        : selectedProject === 'INBOX' || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where('date', '==', ''))
+        : unsubscribe;
+
+    }, []);
+    
+};
 
