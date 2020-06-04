283c14dfb4e05d2b914aee430dd1df227075ddcc
bradleywh1 <bradley.wh@outlook.com>
2020-06-04 14:44:08 +1200

    progress on sidebar and projects components

---
+++ src/components/Projects.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
+
+const export Projects = ({ activeValue = null }) => {
+    const [active, setActive] = useState(activeValue);
+    const { setSelectedProject } = useSelectedProjectValue();
+    const { projects } = useProjectsValue();
+
+    return (
+        projects && 
+        projects.map(project => (
+            <li
+                key={project.projectId}
+                data-doc-id={project.docId}
+                data-test-id="project-action"
+                className={
+                    active === project.projectId
+                        ? 'active_sidebar_project'
+                        : 'sidebar_project'
+                }
+                onClick={() => {
+                    setActive(project.projectId);
+                    setSelectedProject(project.projectId);
+                }}
+            >
+                I am a project...
+            </li>
+        ))
+    )
+}
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa';
+import { useSelectedProjectValue } from '../../context';
 
-export const Sidebar = () => (
-    <div className="sidebar" data-testid="sidebar">
-        <ul className="sidebar__generic">
-            <li datatest-id="inbox" className="inbox">
-                <span>
-                    <FaInbox />
-                </span>
-                <span>Inbox</span>
-            </li>
-            <li datatest-id="today" className="today">
-                <span>
-                    <FaRegCalendar />
-                </span>
-                <span>Today</span>
-            </li>
-            <li datatest-id="next_7" className="next_7">
-                <span>
-                    <FaRegCalendarAlt />
-                </span>
-                <span>Next 7 days</span>
-            </li>
-        </ul>
+export const Sidebar = () => {
+    const { setSelectedProject } = useSelectedProjectValue;
+    const [active, setActive] = useState('inbox');
+    const [showProjects, setShowProjects] = useState(true);
+    
+    return (
+        <div className="sidebar" data-testid="sidebar">
+            <ul className="sidebar__generic">
+                <li datatest-id="inbox" className="inbox">
+                    <span>
+                        <FaInbox />
+                    </span>
+                    <span>Inbox</span>
+                </li>
+                <li datatest-id="today" className="today">
+                    <span>
+                        <FaRegCalendar />
+                    </span>
+                    <span>Today</span>
+                </li>
+                <li datatest-id="next_7" className="next_7">
+                    <span>
+                        <FaRegCalendarAlt />
+                    </span>
+                    <span>Next 7 days</span>
+                </li>
+            </ul>
 
-        <div className="sidebar__middle">
-            <span>
-                <FaChevronDown />
-            </span>
-            <h2>Projects</h2>
+            <div className="sidebar__middle">
+                <span>
+                    <FaChevronDown />
+                </span>
+                <h2>Projects</h2>
+            </div>
+
+            <ul className="sidebar__projects">Projects will go here</ul>
+            Add Project component here!
         </div>
-
-        <ul className="sidebar__projects">Projects will go here</ul>
-        Add Project component here!
-    </div>
-);
+    );
+};
