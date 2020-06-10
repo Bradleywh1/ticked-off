ccee1e2e13a7e84e1072c1cc852999c217c76692
bradleywh1 <bradley.wh@outlook.com>
2020-06-10 16:14:30 +1200

    progress on task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Checkbox } from './Checkbox';
 import { useTasks } from '../hooks';
 import { collatedTasks } from '../constants';
-import { getTitle, getCollatedTitle, collatedTasksExists } from '../helpers';
-import { useTasks } from '../hooks';
-import { useSelectedProjectValue, useProjectsVal, useProjectsValue } from '../context';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Tasks = () => {
     const { selectedProject } = useSelectedProjectValue();
     const { projects } = useProjectsValue();
     const { tasks } = useTasks(selectedProject); 
 
-    const projectName = '';
+    let projectName = '';
+
+    if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+        projectName = getTitle(projects, selectedProject).name;
+        console.log('projectName 1: ', projectName);
+    }
+
+    if (collatedTasksExist(selectedProject) && selectedProject) {
+        projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+        console.log('projectName 2: ', projectName);
+    }
+
+    useEffect(() => {
+        document.title = `${projectName}: Todoist`;
+    })
 
     return (
         <div className="tasks" data-testid="tasks">
